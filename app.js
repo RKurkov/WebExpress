9a7e65d56f7707347939cc5cf4124ddd4c4fafe9
RKurkov <ruskura@icloud.com>
2023-05-27 09:16:18 +0300

    7LR

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
 const express = require('express');
 const restAPI = require('./router/routes');
+const morgan = require("morgan");
+const helmet = require("helmet");
 
 const app = express();
-const PORT = 8080;
+const PORT = 8000;
 const HOST = "127.0.0.1";
 
 app.use(express.static('public'));
 app.use("/v1", restAPI);
 
-app.use((req, res) => {
-	res.status(404).send('Данная страница не найдена!');
-});
+app.use(morgan('dev'));
+app.use(helmet);
 
-app.use((err, req, res) => {
-	res.status(500).send('Ошибка сервера')
+app.use((err, req, res, next) => {
+	if (err.statusCode) {
+		res.status(err.statusCode).send(err.message);
+	}
+	else {
+		res.status(400).json("Отправьте запрос корректно!");
+	}
 });
 
 app.listen(PORT, HOST, () => {
--- router/routes.js
+++ router/routes.js
@@ -5,8 +5,27 @@ const jsonParser = express.json();
 let user = { user_agent: 0 };
 let com = '';
 
+function validation(req, res, next){
+	if(JSON.stringify(req.body) == '{}'){
+		next(error);
+	}else next();
+}
+
+function checkAuthorization(req, res, next){
+	const apiKey = req.query.key;
+	if (apiKey !== '123'){
+		next(err);
+	}
+	else next();
+}
+
+
 router.get("/", (req, res) => {
     res.status(200).set('Content-Type', 'text/plain', 'charset=utf-8').send("Hello!");
+});
+
+router.post('/login', jsonParser, validation, checkAuthorization, (req, res) => {
+	res.send('Успешно!');
 });
 
 router.get("/stats", (req, res) => {
@@ -21,10 +40,15 @@ router.get("/stats", (req, res) => {
 		</table>`);
 });
 
-router.post("/comments", jsonParser, (req, res) => {
+router.post("/comments", jsonParser, validation, (req, res) => {
     console.log(req.body);
 	com += JSON.stringify(req.body);
 	res.status(200).set('Content-Type', 'text/plain', 'charset=utf-8').send('Данные успешно отправлены!');
+});
+
+router.use((req, res, next) =>{
+	res.status(404).json('Такой страницы не существует!');
+	next();
 });
 
 module.exports = router;
